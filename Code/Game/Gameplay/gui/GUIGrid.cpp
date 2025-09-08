c6e5290229a349c7ccbe5f6c2e1b4d860b38df5f
CaIzii <2470159730@qq.com>
2025-09-07 20:16:27 -0500

    Add GUIGrid class with basic lifecycle methods and font initialization

---
+++ Code/Game/Gameplay/gui/GUIGrid.cpp
@@ -0,0 +1,31 @@
+﻿#include "GUIGrid.hpp"
+
+#include "Engine/Core/EngineCommon.hpp"
+#include "Engine/Renderer/IRenderer.hpp"
+#include "Game/GameCommon.hpp"
+
+GUIGrid::GUIGrid() : GUI()
+{
+    g_theRenderer->CreateOrGetBitmapFont("Run/.enigma/data/Fonts/SquirrelFixedFont");
+}
+
+GUIGrid::~GUIGrid()
+{
+}
+
+void GUIGrid::Draw()
+{
+}
+
+void GUIGrid::Update(float deltaTime)
+{
+    UNUSED(deltaTime)
+}
+
+void GUIGrid::OnCreate()
+{
+}
+
+void GUIGrid::OnDestroy()
+{
+}
---
+++ Code/Game/Gameplay/gui/GUIGrid.hpp
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "Game/Framework/GUISubsystem.hpp"
+
+class BitmapFont;
+
+class GUIGrid : public GUI
+{
+    DECLARE_GUI(GUIGrid, "GUIGrid", 100)
+
+public:
+    explicit GUIGrid();
+    ~GUIGrid() override;
+
+    void Draw() override;
+    void Update(float deltaTime) override;
+    void OnCreate() override;
+    void OnDestroy() override;
+
+private:
+    BitmapFont* m_font = nullptr;
+};
