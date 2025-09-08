51b7046b03319d2368cb6012ef8cd4b0c6963e86
CaIzii <2470159730@qq.com>
2025-09-07 20:16:52 -0500

    Add GUISubsystem implementation with GUI rendering, lifecycle management, and viewport handling

---
+++ Code/Game/Framework/GUISubsystem.cpp
@@ -0,0 +1,141 @@
+﻿#include "GUISubsystem.hpp"
+
+#include "Engine/Core/Logger/LoggerAPI.hpp"
+#include "Engine/Renderer/Camera.hpp"
+#include "Engine/Renderer/IRenderer.hpp"
+#include "Game/GameCommon.hpp"
+
+GUISubsystem::GUISubsystem(GUIConfig& config) : m_config(config)
+{
+}
+
+void GUISubsystem::Render()
+{
+    RenderGUI();
+    RenderHud();
+}
+
+void GUISubsystem::Update(float deltaTime)
+{
+    EngineSubsystem::Update(deltaTime);
+    for (auto& gui : m_guis)
+    {
+        if (gui && !gui->m_pendingDestroy)
+            gui->Update(deltaTime);
+    }
+}
+
+void GUISubsystem::EndFrame()
+{
+    EngineSubsystem::EndFrame();
+    for (auto& gui : m_guis)
+    {
+        if (gui && gui->m_pendingDestroy)
+        {
+            gui.reset();
+            gui = nullptr;
+        }
+    }
+}
+
+void GUISubsystem::Startup()
+{
+    LogInfo("GUISubsystem", "Starting up GUISubsystem...");
+    // 3D Camera, use for HUD
+    m_hudCamera         = std::make_unique<Camera>();
+    m_hudCamera->m_mode = eMode_Perspective;
+    m_hudCamera->SetPerspectiveView(2.0f, 60.f, 0.1f, 100.f);
+    Mat44 ndcMatrix;
+    ndcMatrix.SetIJK3D(Vec3(0, 0, 1), Vec3(-1, 0, 0), Vec3(0, 1, 0));
+    m_hudCamera->SetCameraToRenderTransform(ndcMatrix);
+
+    // 2D Camera, use for GUI
+    m_guiCamera         = std::make_unique<Camera>();
+    m_guiCamera->m_mode = eMode_Orthographic;
+    m_guiCamera->SetOrthographicView(m_config.screenSpace.m_mins, m_config.screenSpace.m_maxs);
+
+    // Font
+    m_defaultGUIFont = g_theRenderer->CreateOrGetBitmapFont(".enigma\\data\\Fonts\\SquirrelFixedFont");
+}
+
+void GUISubsystem::Shutdown()
+{
+    LogInfo("GUISubsystem", "Shutdown GUISubsystem...");
+}
+
+void GUISubsystem::RenderHud()
+{
+    g_theRenderer->BeginCamera(*m_hudCamera);
+    for (auto& gui : m_guis)
+    {
+        if (gui && !gui->m_pendingDestroy)
+            gui->DrawHud();
+    }
+    g_theRenderer->EndCamera(*m_hudCamera);
+}
+
+void GUISubsystem::RenderGUI()
+{
+    g_theRenderer->BeginCamera(*m_guiCamera);
+    for (auto& gui : m_guis)
+    {
+        if (gui && !gui->m_pendingDestroy)
+            gui->Draw();
+    }
+    g_theRenderer->EndCamera(*m_guiCamera);
+}
+
+std::shared_ptr<GUI> GUISubsystem::AddToViewPort(std::shared_ptr<GUI> gui)
+{
+    if (gui && !gui->m_hasCreate && !gui->m_pendingDestroy)
+    {
+        m_guis.push_back(gui);
+        gui->m_guiCamera      = m_guiCamera;
+        gui->m_hudCamera      = m_hudCamera;
+        gui->m_config         = m_config;
+        gui->m_defaultGUIFont = m_defaultGUIFont;
+        gui->OnCreate();
+        LogInfo("gui", "Add %s GUI to ViewPort", gui->GetName());
+        return gui;
+    }
+    else
+    {
+        LogError("gui", "Fail to add %s GUI to ViewPort");
+    }
+    return nullptr;
+}
+
+std::shared_ptr<GUI> GUISubsystem::RemoveFromViewPort(std::shared_ptr<GUI> gui)
+{
+    for (std::shared_ptr<GUI>& ptr : m_guis)
+    {
+        if (ptr == gui)
+        {
+            ptr->OnDestroy();
+            ptr->m_pendingDestroy = true;
+            LogInfo("gui", "Remove %s GUI from ViewPort", gui->GetName());
+            return ptr;
+        }
+    }
+    return nullptr;
+}
+
+std::shared_ptr<GUI> GUISubsystem::GetGUI(const std::type_index& type)
+{
+    for (std::shared_ptr<GUI>& gui : m_guis)
+    {
+        if (gui && std::type_index(typeid(*gui)) == type)
+            return gui;
+    }
+    return nullptr;
+}
+
+std::shared_ptr<GUI> GUISubsystem::GetGUI(std::string& name)
+{
+    for (std::shared_ptr<GUI>& gui : m_guis)
+    {
+        if (gui->GetName() == name)
+            return gui;
+    }
+    return nullptr;
+}
---
+++ Code/Game/Framework/GUISubsystem.hpp
@@ -0,0 +1,75 @@
+﻿#pragma once
+#include "Engine/Core/SubsystemManager.hpp"
+#include "Engine/Math/AABB2.hpp"
+
+class BitmapFont;
+class Camera;
+using namespace enigma::core;
+
+struct GUIConfig
+{
+    AABB2 screenSpace = AABB2::ZERO_TO_ONE;
+};
+
+
+class GUI
+{
+    friend class GUISubsystem;
+
+public:
+    virtual             ~GUI() = default;
+    virtual const char* GetName() const = 0;
+    virtual int         GetPriority() const = 0;
+    virtual void        Draw() = 0;
+    virtual void        DrawHud() = 0;
+    virtual void        Update(float deltaTime) = 0;
+    virtual void        OnCreate() = 0;
+    virtual void        OnDestroy() = 0;
+
+#define DECLARE_GUI(ClassName, Name, Priority) \
+    public:\
+            const char* GetName() const override { return Name; } \
+            int GetPriority() const override { return Priority; }
+
+protected:
+    std::shared_ptr<Camera> m_guiCamera      = nullptr;
+    std::shared_ptr<Camera> m_hudCamera      = nullptr;
+    BitmapFont*             m_defaultGUIFont = nullptr;
+    GUIConfig               m_config;
+
+private:
+    bool m_hasCreate      = false;
+    bool m_pendingDestroy = false;
+};
+
+
+class GUISubsystem : public EngineSubsystem
+{
+public:
+    DECLARE_SUBSYSTEM(GUISubsystem, "guiSubsystem", 400)
+
+    explicit GUISubsystem(GUIConfig& config);
+
+    void Render();
+    void Update(float deltaTime) override;
+    void EndFrame() override;
+    void Startup() override;
+    void Shutdown() override;
+
+private:
+    void RenderHud();
+    void RenderGUI();
+
+public:
+    [[maybe_unused]] std::shared_ptr<GUI> AddToViewPort(std::shared_ptr<GUI> gui);
+    [[maybe_unused]] std::shared_ptr<GUI> RemoveFromViewPort(std::shared_ptr<GUI> gui);
+    std::shared_ptr<GUI>                  GetGUI(const std::type_index& type);
+    std::shared_ptr<GUI>                  GetGUI(std::string& name);
+
+private:
+    GUIConfig                         m_config;
+    std::vector<std::shared_ptr<GUI>> m_guis;
+    std::shared_ptr<Camera>           m_guiCamera      = nullptr;
+    std::shared_ptr<Camera>           m_hudCamera      = nullptr;
+    BitmapFont*                       m_defaultGUIFont = nullptr;
+};
