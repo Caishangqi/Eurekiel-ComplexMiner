5e853cdb21e8e9ef5e06fcbda14c8fa2fce2f0f4
CaIzii <2470159730@qq.com>
2025-09-07 20:16:32 -0500

    Add GUICrosser class and Crosser struct with rendering and update functionality

---
+++ Code/Game/Gameplay/gui/GUICrosser.cpp
@@ -0,0 +1,73 @@
+﻿#include "GUICrosser.hpp"
+
+#include "Engine/Core/EngineCommon.hpp"
+#include "Engine/Core/VertexUtils.hpp"
+#include "Engine/Renderer/Camera.hpp"
+#include "Engine/Renderer/IRenderer.hpp"
+#include "Game/GameCommon.hpp"
+#include "Game/Gameplay/entity/Player.hpp"
+
+Crosser::Crosser()
+{
+    AddVertsForArrow3D(m_vertexes, Vec3(0.1f, 0, 0), Vec3(), 0.004f, 0.4f, Rgba8::RED);
+    AddVertsForArrow3D(m_vertexes, Vec3(0, 0.1f, 0), Vec3(), 0.004f, 0.4f, Rgba8::GREEN);
+    AddVertsForArrow3D(m_vertexes, Vec3(0, 0, 0.1f), Vec3(), 0.004f, 0.4f, Rgba8::BLUE);
+}
+
+void Crosser::Update(float deltaTime)
+{
+    UNUSED(deltaTime)
+}
+
+Mat44 Crosser::GetModelToWorldTransform()
+{
+    Mat44 matTranslation = Mat44::MakeTranslation3D(m_position);
+    matTranslation.Append(m_orientation.GetAsMatrix_IFwd_JLeft_KUp());
+    matTranslation.Append(Mat44::MakeNonUniformScale3D(m_scale));
+    return matTranslation;
+}
+
+void Crosser::Renderer()
+{
+    g_theRenderer->SetDepthMode(DepthMode::DISABLED);
+    g_theRenderer->SetModelConstants(GetModelToWorldTransform(), m_color);
+    g_theRenderer->SetBlendMode(BlendMode::ALPHA);
+    g_theRenderer->BindTexture(nullptr);
+    g_theRenderer->DrawVertexArray(m_vertexes);
+}
+
+GUICrosser::GUICrosser(Player* player) : GUI(), m_player(player)
+{
+    m_crosser             = std::make_unique<Crosser>();
+    m_crosser->m_position = m_player->m_position;
+}
+
+void GUICrosser::Draw()
+{
+}
+
+void GUICrosser::DrawHud()
+{
+    m_crosser->Renderer();
+}
+
+void GUICrosser::Update(float deltaTime)
+{
+    m_crosser->Update(deltaTime);
+    Vec3 iFwd, jLeft, kUp;
+    m_player->m_orientation.GetAsVectors_IFwd_JLeft_KUp(iFwd, jLeft, kUp);
+    float offsetDistance  = 2.f;
+    Vec3  debugAxisPos    = m_player->m_position + iFwd * offsetDistance;
+    m_crosser->m_position = debugAxisPos;
+
+    m_hudCamera->SetPosition(m_player->m_position);
+    m_hudCamera->SetOrientation(m_player->m_orientation);
+}
+
+void GUICrosser::OnCreate()
+{
+}
+
+void GUICrosser::OnDestroy()
+{
+}
---
+++ Code/Game/Gameplay/gui/GUICrosser.hpp
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include "Engine/Core/Rgba8.hpp"
+#include "Engine/Math/EulerAngles.hpp"
+#include "Engine/Math/Vec3.hpp"
+#include "Game/Framework/GUISubsystem.hpp"
+
+
+struct Vertex_PCU;
+class Player;
+
+struct Crosser
+{
+    explicit Crosser();
+
+    void  Update(float deltaTime);
+    Mat44 GetModelToWorldTransform();
+    void  Renderer();
+
+    std::vector<Vertex_PCU> m_vertexes;
+    Vec3                    m_position = Vec3::ZERO;
+    EulerAngles             m_orientation;
+    Vec3                    m_scale = Vec3::ONE;
+    Rgba8                   m_color = Rgba8::WHITE;
+};
+
+class GUICrosser : public GUI
+{
+    DECLARE_GUI(GUICrosser, "GUICrosser", 100)
+
+public:
+    explicit GUICrosser(Player* player);
+
+    void Draw() override;
+    void DrawHud() override;
+    void Update(float deltaTime) override;
+    void OnCreate() override;
+    void OnDestroy() override;
+
+private:
+    Player*                  m_player  = nullptr;
+    std::shared_ptr<Crosser> m_crosser = nullptr;
+};
